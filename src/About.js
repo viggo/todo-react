39d9cea037c29eea120293d5137b8db90cb8c320
Viggo Normann <v@iggo.no>
2022-02-01 16:14:39 +0100

    Add dark mode and toggle button to about pages

--- src/About.js
+++ src/About.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { TodoContext  } from './components/TodoContext';
+import ToggleDark from './components/ToggleDark';
 
 export default function About() {
+  const { dark } = useContext(TodoContext)
   return (
-    <div className="todoapp stack-large">
+    <div className="todoapp stack-large" style={{ backgroundColor: dark && 'black', color: dark && 'white'}}>
       <main>
         <Outlet />
       </main>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/" style={{ color: dark && 'white'}}>Home</Link>
       </nav>
+      <ToggleDark />
     </div>
   );
 }
--- src/AboutIndex.js
+++ src/AboutIndex.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { TodoContext  } from './components/TodoContext';
 
 export default function AboutIndex() {
+  const { dark } = useContext(TodoContext)
   return (
     <>
       <h2>Hvem er vi?</h2>
       <p>
-        <Link to="Viggo">Viggo</Link>
+        <Link to="Viggo" style={{ color: dark && 'white'}}>Viggo</Link>
       </p>
       <p>
-        <Link to="Kent">Kent</Link>
+        <Link to="Kent" style={{ color: dark && 'white'}}>Kent</Link>
       </p>
     </>
   );
